77712833f6b2bb4a9b47decc08ffb2106eca957c
Eric Zwierzynski <123706130+ericshark@users.noreply.github.com>
2024-12-07 21:30:35 -0500

    completion

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "/"];
 
 
+btn1El = document.getElementById("output-1")
+btn2El = document.getElementById("output-2")
+
+
+function generate(){
+    let result = ""
+    for(let i = 0; i<15; i++){
+        rand = Math.floor(Math.random() * characters.length)
+        result+= characters[rand] 
+    }
+    return result
+}
+
+
+function create(){
+    btn1El.textContent =  generate()
+    btn2El.textContent = generate()
+}
